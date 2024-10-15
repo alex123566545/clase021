8ba4075c484f5c2c87aba7f9a30581b974d47b40
alex123566545 <139175706+alex123566545@users.noreply.github.com>
2024-10-14 21:43:11 -0500

    e3

--- sem02.py
+++ sem02.py
@@ -10,5 +10,10 @@ if st.button("Ejecutar E1"):
 
 st.subheader("Ejercicio 2: imprimir los 10 primeros numeros")
 if st.button("Ejecutar E2"):
-    for i in range(1, 13):
+    for i in range(1, 11):
         st.write(i)
+st.subheader("Ejercicio 3: imprimir la tabla de multiplicar del numero ingresado")
+num = st.number_input("Ingrese un numero para ver su tabla de multiplicar del 1 al 12", min_value = 1, max_value=12)
+if st.button("Ejecutar Ejercicio 3"):
+    for i in range(1,13):
+        st.write(f"{num} x {i} ={num*i}")
