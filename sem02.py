ab9bb666d7bad69cc7b6fecd6f8e31ef38afd7fd
alex123566545 <139175706+alex123566545@users.noreply.github.com>
2024-10-14 21:27:03 -0500

    cambiando de ejercicio

--- sem02.py
+++ sem02.py
@@ -1,36 +1,9 @@
 import streamlit as st
 
 # Inicialización de variables en el estado de la sesión
-if 'usuarios' not in st.session_state:
-    st.session_state['usuarios'] = []
+st.title("Ejercicios basicos con bucles") 
 
-# Función para agregar un usuario
-def agregar_usuario(nombre):
-    st.session_state['usuarios'].append(nombre)
-    st.success(f"Usuario {nombre} agregado.")
-
-# Función para mostrar usuarios
-def mostrar_usuarios():
-    if st.session_state['usuarios']:
-        st.write("Lista de usuarios:")
-        for usuario in st.session_state['usuarios']:
-            st.write(f"- {usuario}")
-    else:
-        st.warning("No hay usuarios registrados.")
-
-# Menú Principal
-st.title("Gestión de Usuarios")
-
-opcion = st.selectbox("Selecciona una opción", ["Agregar Usuario", "Mostrar Usuarios"])
-
-if opcion == "Agregar Usuario":
-    nombre = st.text_input("Nombre del usuario")
-    if st.button("Agregar"):
-        if nombre:
-            agregar_usuario(nombre)
-        else:
-            st.error("El nombre no puede estar vacío")
-
-elif opcion == "Mostrar Usuarios":
-    if st.button("Mostrar usuarios"):
-        mostrar_usuarios()
+st.subheader("Ejercicio 1: imprimir hola mundo 9 veces")
+if st.button("Ejecutar E1"):
+    for i in range(10):
+        st.write("hola mundo")
