3dcde4320817af139162d248c31471d0246a1afe
alex123566545 <139175706+alex123566545@users.noreply.github.com>
2024-10-14 21:34:04 -0500

    e2

--- sem02.py
+++ sem02.py
@@ -7,3 +7,9 @@ st.subheader("Ejercicio 1: imprimir hola mundo 9 veces")
 if st.button("Ejecutar E1"):
     for i in range(10):
         st.write("hola mundo")
+
+st.subheader("Ejercicio 2: imprimir los 10 primeros numeros")
+
+if st.button("Ejecutar E2"):
+    for i in range(1,11);
+    st.write(i);
